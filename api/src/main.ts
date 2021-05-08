7ce8786f60d1c6d4b517c1bba1f7fc936c11e164
claudealdric <cvaldric@gmail.com>
2021-05-08 00:59:18 -0400

    Add valid origins for CORS

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: 'http://localhost:3000',
+      origin: [
+        'http://localhost:3000',
+        'https://twitter-clone-nu-five.vercel.app/',
+        'https://twitter-clone-ragyj3114-claudealdric.vercel.app/',
+      ],
     },
   });
   await app.listen(8080);
