c13c1a135d27a741b3c02f080cb7b31787872238
claudealdric <cvaldric@gmail.com>
2021-05-08 01:26:58 -0400

    Revert API URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,7 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const getHelloWorld = async () => {
-      const url = 'http://localhost:8080'
+      const url = process.env.REACT_APP_API_URL || 'http://localhost:8080'
       const { data } = await axios.get<string>(url)
       setSampleText(data)
     }
