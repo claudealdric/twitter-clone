3b37a4e8088186eeefe1cbff05aae74164eb58eb
claudealdric <cvaldric@gmail.com>
2021-05-08 01:23:21 -0400

    Set CORS to true

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,15 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://twitter-clone-nu-five.vercel.app/',
-        'https://twitter-clone-ragyj3114-claudealdric.vercel.app/',
-      ],
-    },
-  });
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(8080);
 }
 bootstrap();
