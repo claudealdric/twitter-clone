f351508738e3cee56b944010cb1fbd463aad1fc0
claudealdric <cvaldric@gmail.com>
2021-05-08 00:34:18 -0400

    Integrate .env variable into API URL

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,7 +17,8 @@ const App: React.FC = () => {
 
   useEffect(() => {
     const getHelloWorld = async () => {
-      const { data } = await axios.get<string>('http://localhost:8080')
+      const url = process.env.REACT_APP_API_URL || 'http://localhost:8080'
+      const { data } = await axios.get<string>(url)
       setSampleText(data)
     }
 
