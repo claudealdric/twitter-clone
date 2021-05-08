34c42c283817b58f8538a2d63761808a2204eb7f
claudealdric <cvaldric@gmail.com>
2021-05-07 22:48:59 -0400

    Add Redux toolkit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from './store'
+import {
+  increment,
+  incrementAsync,
+  decrement,
+  decrementAsync,
+} from './slices/counter.slice'
 
-function App() {
+const App: React.FC = () => {
+  const dispatch = useDispatch()
+  const counter = useSelector((state: RootState) => state.counter)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Counter: {counter}</p>
+      <button type="button" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <button type="button" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+      <button type="button" onClick={() => dispatch(incrementAsync())}>
+        IncrementAsync
+      </button>
+      <button type="button" onClick={() => dispatch(decrementAsync())}>
+        DecrementAsync
+      </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ client/src/slices/counter.slice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { AppThunk } from '../store';
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    increment: (count: number) => count + 1,
+    decrement: (count: number) => count - 1,
+  },
+});
+
+export const { reducer } = counterSlice;
+export const { increment, decrement } = counterSlice.actions;
+
+// Thunks
+export const incrementAsync = (): AppThunk => (dispatch) => {
+  setTimeout(() => dispatch(increment()), 1000);
+};
+
+export const decrementAsync = (): AppThunk => (dispatch) => {
+  setTimeout(() => dispatch(decrement()), 1000);
+};
