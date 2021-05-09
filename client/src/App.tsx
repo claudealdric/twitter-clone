6232e939f68e6e29bb827165a0465684369d8ccc
claudealdric <cvaldric@gmail.com>
2021-05-09 11:37:49 -0400

    Remove unneeded code blocks

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,9 @@
-import axios from 'axios'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect, useState } from 'react'
 
 import LoginPage from './auth/LoginPage'
 import RegisterPage from './auth/RegisterPage'
@@ -16,29 +13,7 @@ import Navbar from './feed/Navbar'
 
 import LandingPage from './LandingPage'
 
-import { RootState } from './store'
-// import {
-//   increment,
-//   incrementAsync,
-//   decrement,
-//   decrementAsync,
-// } from './slices/counter.slice'
-
 const App: React.FC = () => {
-  const dispatch = useDispatch()
-  const counter = useSelector((state: RootState) => state.counter)
-  const [sampleText, setSampleText] = useState('')
-
-  useEffect(() => {
-    const getHelloWorld = async () => {
-      const url = process.env.REACT_APP_API_URL || 'http://localhost:8080'
-      const { data } = await axios.get<string>(url)
-      setSampleText(data)
-    }
-
-    getHelloWorld()
-  }, [])
-
   return (
     <div>
       <Router>
@@ -60,21 +35,6 @@ const App: React.FC = () => {
           <Redirect to="/auth/login" />
         </Route>
       </Router>
-      {/* <Routes /> */}
-      {/* <p>API Response: {sampleText}</p>
-      <p>Counter: {counter}</p>
-      <button type="button" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <button type="button" onClick={() => dispatch(decrement())}>
-        Decrement
-      </button>
-      <button type="button" onClick={() => dispatch(incrementAsync())}>
-        IncrementAsync
-      </button>
-      <button type="button" onClick={() => dispatch(decrementAsync())}>
-        DecrementAsync
-      </button> */}
     </div>
   )
 }
