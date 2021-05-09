5bb064384233184452df52ac9a0b3dbfccba7e9c
claudealdric <cvaldric@gmail.com>
2021-05-09 12:09:06 -0400

    Specify type to function component

--- client/src/auth/LoginPage.tsx
+++ client/src/auth/LoginPage.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 import bean from '../images/bean.png'
 
-const LoginPage = () => {
+const LoginPage: React.FC = () => {
   const styles: { [key: string]: React.CSSProperties } = {
     container: {
       display: 'flex',
