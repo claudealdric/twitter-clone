506ce110524ac3dc9cfbcb9b45db7927deca9169
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-10 12:45:59 -0400

    Add CSS module to Feed and center tweet box

--- client/src/pages/home/Feed.tsx
+++ client/src/pages/home/Feed.tsx
@@ -8,20 +8,23 @@ import {
   Paper,
 } from '@material-ui/core'
 
+import styles from './Feed.module.css'
+
 const FeedPage: React.FC = () => {
   return (
-    <Grid item xs={10}>
-      <Paper elevation={2}>
-        <form>
-          <FormControl fullWidth>
-            <Input id="tweet-input" placeholder="What's happening?" />
-          </FormControl>
-          <FormControl fullWidth>
-            <Input type="submit" value="Tweet"></Input>
-          </FormControl>
-        </form>
-      </Paper>
-      {/* {tweets.map((tweet) => (
+    <div className={styles.root}>
+      <Grid item xs={11}>
+        <Paper elevation={2}>
+          <form>
+            <FormControl fullWidth>
+              <Input id="tweet-input" placeholder="What's happening?" />
+            </FormControl>
+            <FormControl fullWidth>
+              <Input type="submit" value="Tweet"></Input>
+            </FormControl>
+          </form>
+        </Paper>
+        {/* {tweets.map((tweet) => (
         <Box key={tweet._id} padding={1}>
           <Paper elevation={1}>
             <Box padding={1}>@{tweet.user.handle}</Box>
@@ -29,7 +32,8 @@ const FeedPage: React.FC = () => {
           </Paper>
         </Box>
       ))} */}
-    </Grid>
+      </Grid>
+    </div>
   )
 }
 
