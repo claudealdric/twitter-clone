021e211bea3992da6b07d55ff65e722f25c49cda
Sandy Dai <sandydai@sandys-imac.myfiosgateway.com>
2021-05-10 18:35:43 -0400

    Add avatar header to Profile page

--- client/src/pages/home/Profile.tsx
+++ client/src/pages/home/Profile.tsx
@@ -1,7 +1,25 @@
-import { Grid } from '@material-ui/core'
+import { Typography, Avatar } from '@material-ui/core'
+import styles from './Profile.module.css'
 
 const Profile: React.FC = () => {
-  return <Grid item xs={12} md={12} lg={3}></Grid>
+  return (
+    <div className={styles.root}>
+      <Avatar
+        className={styles.avatar}
+        alt="Bob"
+        src="https://picsum.photos/500"
+      />
+      <div className={styles.header}>
+        <h1 className={styles.username}>@beanadmin</h1>
+        <Typography
+          style={{ color: '#5b7083', marginLeft: '20px' }}
+          component="h6"
+        >
+          Total tweets:
+        </Typography>
+      </div>
+    </div>
+  )
 }
 
 export default Profile
