2fdeb261439b29752d7596ec612fb80a3866e499
claudealdric <cvaldric@gmail.com>
2021-05-11 19:29:33 -0400

    Create login method

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly service: AuthService) {}
+
+  @Post('login')
+  login() {
+    return this.service.login()
+  }
 }
