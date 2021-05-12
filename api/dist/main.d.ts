cd2e4e448db0d06ce15a7e49ec9f1d1465329093
claudealdric <cvaldric@gmail.com>
2021-05-12 09:02:29 -0400

    Fix failing function invocation

--- api/dist/main.d.ts
+++ api/dist/main.d.ts
@@ -1,1 +1,2 @@
 import 'dotenv/config';
+export declare function bootstrap(): Promise<void>;
--- api/dist/main.js
+++ api/dist/main.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.bootstrap = void 0;
 require("dotenv/config");
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
@@ -8,5 +9,6 @@ async function bootstrap() {
     const port = parseInt(process.env.PORT, 10) || 8080;
     await app.listen(port);
 }
+exports.bootstrap = bootstrap;
 bootstrap();
 //# sourceMappingURL=main.js.map
