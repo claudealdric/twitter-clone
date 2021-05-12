aa01ccc22819a17cfe362ebbd48961c4499caaef
claudealdric <cvaldric@gmail.com>
2021-05-12 08:50:50 -0400

    Fix failing function invocation

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core'
 
 import { AppModule } from './app.module'
 
-export async function bootstrap() {
+async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true })
   const port = parseInt(process.env.PORT, 10) || 8080
   await app.listen(port)
