fa1b84fc19bff5cccf0a90890a7a134df8c1c1a5
claudealdric <cvaldric@gmail.com>
2021-05-11 23:04:23 -0400

    Attach state to login form

--- client/src/pages/auth/Login.tsx
+++ client/src/pages/auth/Login.tsx
@@ -1,10 +1,21 @@
 import { Button, FormControl, Grid, Input } from '@material-ui/core'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 import bean from 'images/bean.jpg'
 import styles from './Login.module.css'
 
 const Login: React.FC = () => {
+  const [handle, setHandle] = useState('')
+  const [password, setPassword] = useState('')
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    // Prevent page refresh
+    event.preventDefault()
+
+    console.table({ handle, password })
+  }
+
   return (
     <div className={styles.container}>
       <img className={styles.image} src={bean} alt="bean cartoon"></img>
@@ -16,12 +27,14 @@ const Login: React.FC = () => {
               {/* Empty grid for spacing */}
             </Grid>
             <Grid>
-              <form>
+              <form onSubmit={onSubmit}>
                 <FormControl fullWidth>
                   <Input
                     className={styles.input}
                     id="handle"
                     placeholder="Handle"
+                    value={handle}
+                    onChange={(event) => setHandle(event.target.value)}
                   />
                 </FormControl>
                 <FormControl fullWidth>
@@ -30,10 +43,16 @@ const Login: React.FC = () => {
                     type="password"
                     id="password"
                     placeholder="Password"
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}
                   />
                 </FormControl>
                 <FormControl fullWidth>
-                  <Button variant="contained" className={styles.button}>
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    className={styles.button}
+                  >
                     Log in
                   </Button>
                 </FormControl>
