e769be2686118cb171b3eb14927e63b0cc6ee95b
claudealdric <cvaldric@gmail.com>
2021-05-13 23:08:46 -0400

    Create user slice

---
+++ client/src/data/slices/user.slice.ts
@@ -0,0 +1,41 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+import { AppThunk } from 'data/store'
+import { usersEndpoint } from 'api'
+
+enum UserLoadingStates {
+  pending = 'pending',
+  idle = 'idle',
+}
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState: {
+    user: { handle: '', fullName: '' },
+    loading: UserLoadingStates.idle,
+  },
+  reducers: {
+    userLoading: (state) => {
+      if (state.loading === UserLoadingStates.idle) {
+        state.loading = UserLoadingStates.pending
+      }
+    },
+    userReceived: (state, action) => {
+      if (state.loading === UserLoadingStates.pending) {
+        state.user = action.payload
+        state.loading = UserLoadingStates.idle
+      }
+    },
+  },
+})
+
+// Exports
+export const { reducer } = userSlice
+export const { userLoading, userReceived } = userSlice.actions
+
+// Thunks
+export const getUser = (handle: string): AppThunk => async (dispatch) => {
+  dispatch(userLoading())
+  const { data } = await usersEndpoint.get(`/${handle}`)
+  dispatch(userReceived(data))
+}
