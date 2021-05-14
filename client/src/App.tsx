d0ba464cf1bf0c4478eb616e6766ea02500bd4ed
claudealdric <cvaldric@gmail.com>
2021-05-14 00:47:13 -0400

    Attach user state and add protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import { Router, Redirect, Route, Switch } from 'react-router-dom'
+import { useSelector } from 'react-redux'
 
 import FeedPage from './pages/home/Feed'
 import Followers from './pages/home/Followers'
@@ -8,21 +9,34 @@ import Navbar from './components/shared/Navbar'
 import Profile from './pages/home/Profile'
 import Register from './pages/auth/Register'
 import history from 'utils/history'
+import { RootState } from 'data/store'
 
 const App: React.FC = () => {
+  const user = useSelector((state: RootState) => state.user)
+  const loggedIn = () => Boolean(user.user.handle)
+
   return (
     <Router history={history}>
       <Navbar />
       <Switch>
+        {/* Non-protected routes */}
         <Route exact path="/">
           <Redirect to="/auth/login" />
         </Route>
         <Route exact path="/auth/login" component={Login} />
         <Route exact path="/auth/register" component={Register} />
-        <Route exact path="/followers" component={Followers} />
-        <Route exact path="/following" component={Following} />
         <Route exact path="/home" component={FeedPage} />
-        <Route exact path="/profile" component={Profile} />
+
+        {/* Protected routes  */}
+        <Route exact path="/followers">
+          {loggedIn() ? <Followers /> : <Redirect to="/auth/login" />}
+        </Route>
+        <Route exact path="/following">
+          {loggedIn() ? <Following /> : <Redirect to="/auth/login" />}
+        </Route>
+        <Route exact path="/profile">
+          {loggedIn() ? <Profile /> : <Redirect to="/auth/login" />}
+        </Route>
       </Switch>
     </Router>
   )
